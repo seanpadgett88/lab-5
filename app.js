991eb6ac765a496d57fead56b00111b5e6a3ca8d
Sean Padgett <sean.padgett88@yahoo.com>
2017-08-11 16:50:21 -0700

    Finished question 2

--- app.js
+++ app.js
@@ -70,11 +70,16 @@ Test this function by hand in the console to get it working, and when you think 
 // TODO: Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 function sumArray(testArray){ //eslint-disable-line
-
+  var zeroPlace = testArray[0];
+  var firstPlace = testArray[1];
+  var secondPlace = testArray[2];
+  var sumOfThree = sum(sum(zeroPlace, firstPlace)[0], secondPlace)[0];
+  var sumString = zeroPlace + ',' + firstPlace + ',' + secondPlace + ' was passed in as an array of numbers, and ' + sumOfThree + ' is their sum.';
+  return [sumOfThree, sumString];
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
